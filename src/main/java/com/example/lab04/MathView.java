9357ae0e80539dd0a80a05345f9b8bfea06968a9
Admin <63070177@kmitl.ac.th>
2022-09-06 17:17:05 +0700

    12

--- src/main/java/com/example/lab04/MathView.java
+++ src/main/java/com/example/lab04/MathView.java
@@ -1,38 +1,83 @@
 package com.example.lab04;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Route(value = "index1")
 public class MathView extends VerticalLayout {
-
+    private TextField t1, t2, t3;
+    private Button btn1, btn2, btn3, btn4, btn5, btn6;
     public MathView() {
-        TextField num1 = new TextField();
-        num1.setLabel("Number 1");
-        num1.setWidth("500px");
+        t1 = new TextField("Number1");
+        t2 = new TextField("Number2");
+        t3 = new TextField("Answer");
 
-        TextField num2 = new TextField();
-        num2.setLabel("Number 2");
-        num2.setWidth("500px");
+        btn1 = new Button("+");
+        btn2 = new Button("-");
+        btn3 = new Button("*");
+        btn4 = new Button("/");
+        btn5 = new Button("Mod");
+        btn6 = new Button("Max");
 
-        Button plus = new Button("+");
-        Button minus = new Button("-");
-        Button multi = new Button("x");
-        Button divide = new Button("/");
-        Button mod = new Button("mod");
-        Button max = new Button("max");
-        Label lb = new Label("Operator");
-        HorizontalLayout h1 = new HorizontalLayout();
+        HorizontalLayout hl = new HorizontalLayout();
+        VerticalLayout vl = new VerticalLayout();
 
-        TextField ans = new TextField();
-        ans.setLabel("Answer");
-        ans.setWidth("500px");
+        hl.add(btn1, btn2, btn3, btn4, btn5, btn6);
+        vl.add(t1, t2);
+        this.add(vl, hl, t3);
 
-        h1.add(plus, minus, multi, divide, mod, max);
-        add(num1, num2, lb, h1, ans);
+        btn1.addClickListener(event ->{
+            double num1 = Double.parseDouble(t1.getValue());
+            double num2 = Double.parseDouble(t2.getValue());
+
+            String out = WebClient.create().get().uri("http://localhost:8080/plus/"+num1+"/"+num2).retrieve().bodyToMono(String.class).block();
+            t3.setValue(out);
+        });
+
+        btn2.addClickListener(event ->{
+            double num1 = Double.parseDouble(t1.getValue());
+            double num2 = Double.parseDouble(t2.getValue());
+
+            String out = WebClient.create().get().uri("http://localhost:8080/minus/"+num1+"/"+num2).retrieve().bodyToMono(String.class).block();
+            t3.setValue(out);
+        });
+
+        btn4.addClickListener(event ->{
+            double num1 = Double.parseDouble(t1.getValue());
+            double num2 = Double.parseDouble(t2.getValue());
+
+            String out = WebClient.create().get().uri("http://localhost:8080/divide/"+num1+"/"+num2).retrieve().bodyToMono(String.class).block();
+            t3.setValue(out);
+        });
+
+        btn3.addClickListener(event ->{
+            double num1 = Double.parseDouble(t1.getValue());
+            double num2 = Double.parseDouble(t2.getValue());
+
+            String out = WebClient.create().get().uri("http://localhost:8080/multi/"+num1+"/"+num2).retrieve().bodyToMono(String.class).block();
+            t3.setValue(out);
+        });
+
+        btn5.addClickListener(event ->{
+            double num1 = Double.parseDouble(t1.getValue());
+            double num2 = Double.parseDouble(t2.getValue());
+
+            String out = WebClient.create().get().uri("http://localhost:8080/mod/"+num1+"/"+num2).retrieve().bodyToMono(String.class).block();
+            t3.setValue(out);
+        });
+
+        btn6.addClickListener(event ->{
+            double num1 = Double.parseDouble(t1.getValue());
+            double num2 = Double.parseDouble(t2.getValue());
+
+            String out = WebClient.create().post().uri("http://localhost:8080/max/?n1="+num1+"&n2="+num2).retrieve().bodyToMono(String.class).block();
+            t3.setValue(out);
+        });
     }
 }
